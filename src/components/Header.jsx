48046bad7712c373acaee205c179457363616260
Ben <benviscosi@gmail.com>
2024-03-10 15:21:37 -0400

    fix hamburger menu name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,17 @@ import { useLocation } from "react-router-dom";
 import { brainwave } from "../assets";
 import { navigation } from "../constants";
 import Button from "./Button";
+import MenuSvg from "../assets/svg/MenuSvg";
+import { HamburgerMenu } from "./design/Header";
+import { useState } from "react";
 
 const Header = () => {
   const pathname = useLocation();
+  const [openNavigation, setOpenNavigation] = useState(false);
+
   return (
-    <div className="fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
-      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
+    <div className={`fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
+      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4 ${openNavigation} ? 'bg-n-8' : 'bg-n-8/90 backdrop-blur-sm'`}>
         <a className="block w-[12rem] xl:mr-8" href="#hero">
           <img src={brainwave} width={190} height={40} alt="Brainwave" />
         </a>
