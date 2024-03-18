9e3ccd846ce1c1610c987c412874643c9e0f0877
Ben <benviscosi@gmail.com>
2024-03-18 17:34:23 -0400

    put iconUrl back

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -25,7 +25,7 @@ const Benefits = () => {
                 <h5 className="h5 mb-5">{item.title}</h5>
                 <p className="body-2 mb-6 text-n-3">{item.text}</p>
                 <div className="flex items-center mt-auto">
-                  <img src={item.backgroundUrl} width={48} height={48} alt={item.title} />
+                  <img src={item.iconUrl} width={48} height={48} alt={item.title} />
                   <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
                     Explore more
                   </p>
