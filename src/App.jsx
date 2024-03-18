ab3a3a096e765a90ae82601c744c37dce68c144f
Ben <benviscosi@gmail.com>
2024-03-18 15:39:00 -0400

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
 import Roadmap from "./components/Roadmap";
 import Services from "./components/Services";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,9 @@ import Section from "./Section";
 const Footer = () => {
   return (
     <Section crosses className="!px-0 !py-10">
-      <div className="container flex sm:justify-between justify-center items-center gap-10 max-sm:flex-col"></div>
+      <div className="container flex sm:justify-between justify-center items-center gap-10 max-sm:flex-col">
+        <p>{new Date.getFullYear()} All rights reserved</p>
+      </div>
     </Section>
   );
 };
