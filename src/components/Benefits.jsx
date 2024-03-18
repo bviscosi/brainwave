b5d7f8977cf78d2f6f7ab8f7ec452179aaebb7b3
Ben <benviscosi@gmail.com>
2024-03-18 17:04:17 -0400

    test

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -22,11 +22,11 @@ const Benefits = () => {
           title="Chat Smarter, Not Harder with Brainwave"
         />
         <div className="flex flex-wrap gap-10 mb-10">
-          {benefits.map((item) => (
+          {benefits.map((item, index) => (
             <div
               className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
               style={{
-                backgroundImage: `url(${item.backgroundUrl})`,
+                backgroundImage: `url(${benefitCard1})`,
               }}
               key={item.id}>
               <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
