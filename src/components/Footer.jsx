09b7495f101abbf1c75b27bbc9bc18e8cf992ca8
Ben <benviscosi@gmail.com>
2024-03-18 15:35:52 -0400

    add button to bottom of roadmap section, create footer boilerplate

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import Section from "./Section";
+
+const Footer = () => {
+  return (
+    <Section crosses className="!px-0 !py-10">
+      <div className="container flex sm:justify-between justify-center items-center gap-10 max-sm:flex-col"></div>
+    </Section>
+  );
+};
+
+export default Footer;
--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -3,6 +3,7 @@ import Heading from "./Heading";
 import { roadmap } from "../constants";
 import { check2, grid, loading1 } from "../assets";
 import Tagline from "./Tagline";
+import Button from "./Button";
 
 const Roadmap = () => {
   return (
@@ -53,6 +54,9 @@ const Roadmap = () => {
               </div>
             );
           })}
+        </div>
+        <div className="flex justify-center mt-12 md:mt-15 xl:mt-20">
+          <Button href="/roadmap">Our roadmap</Button>
         </div>
       </div>
     </Section>
